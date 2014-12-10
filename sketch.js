550fde86941942d7e2a911cda57a9d362d065f95
seungeon <seungeon.k@gmail.com>
2014-12-10 20:31:38 +0900

    dd

--- sketch.js
+++ sketch.js
@@ -214,10 +214,10 @@ function draw()
     }
 
     fill(255,255,255,80);
-    ellipse(sin(time*2*PI)*100+bubbleX1, bubbleY1,bubbleY1/10,bubbleY1/10);
-    bubbleY1 = bubbleY1 - 7;
-    if(bubbleY1 < 0){
-        bubbleY1 = 900;
+    ellipse(sin(time*2*PI)*100+bubbleX1, bubbleY,bubbleY/10,bubbleY/10);
+    bubbleY = bubbleY - 7;
+    if(bubbleY < 0){
+        bubbleY = 800;
     }
 
     //
